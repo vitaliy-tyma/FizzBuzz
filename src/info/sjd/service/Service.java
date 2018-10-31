3d8bfba8c4ed9f545df1304174074c2ebb9c3534
Vitaliy-tyma <vitaliy.tymashkov@gmail.com>
2018-10-31 12:14:45 +0200

    FizzBuzz

--- src/info/sjd/service/Service.java
+++ src/info/sjd/service/Service.java
@@ -1,10 +1,6 @@
 package info.sjd.service;
 
 public class Service {
-//   private int max;
-//    public Service(int max) {
-//        System.out.println("******************* Service Started *******************");
-//    }
     public static void FizzBuzz(int max) {
 
         StringBuilder sb = new StringBuilder();
@@ -22,8 +18,6 @@ public class Service {
             }
             System.out.println(sb);
             sb.setLength(0);
-
-
         }
     }
 }
